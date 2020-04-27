cea7c0d25aed48811faf27ca9c753c7d2becdd19
Mulk Abdulhadi <mabdulul@gmail.com>
2020-04-27 13:55:52 -0400

    added more and fixed some stuff

--- src/components/List/list.jsx
+++ src/components/List/list.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import uuid from "react-uuid";
+import moment from "moment";
 
 const List = () => {
 	const [ListArray, setListArray] = useState([]);
 	const [task, setTask] = useState("");
+	const [plusAddTaskView, setplusAddTaskView] = useState(true);
+
+	const day = moment();
+	const today = day.format("MMM Do");
+	const theDayWeek = day.format("ddd MMM Do");
 
 	const AddCheckBox = (e) => {
 		e.preventDefault();
@@ -26,29 +32,38 @@ const List = () => {
 
 	return (
 		<div>
+			<h2>Inbox</h2>
 			{ListArray.map((i) => (
-				<>
-					<li key={i.key}>
-						<input type="checkbox" value={i.key} onClick={completeItem} />
-						<label for="vehicle1">{i.text}</label>
-						{/* <button value={i.key} onClick={completeItem}>
-							Done
-						</button> */}
-					</li>
-				</>
+				<li key={i.key}>
+					<input type="checkbox" value={i.key} onClick={completeItem} />
+					<label>{i.text}</label>
+				</li>
 			))}
-			<form onSubmit={AddCheckBox}>
-				<div>
-					<label></label>
-					<input
-						type="text"
-						value={task}
-						onChange={(e) => setTask(e.target.value)}
-						placeholder="e.g Learn Portuguese in Atlanta"
-					></input>
-				</div>
-				<button type="submit">Add Task</button>
-			</form>
+			{!!plusAddTaskView ? (
+				<>
+					<div>test true</div>
+					<button onClick={() => setplusAddTaskView(false)}>Show Form</button>
+				</>
+			) : (
+				<form onSubmit={AddCheckBox}>
+					<div>
+						<label></label>
+						<input
+							type="text"
+							value={task}
+							onChange={(e) => setTask(e.target.value)}
+							placeholder="e.g Learn Portuguese in Atlanta"
+						></input>
+					</div>
+					<span>
+						Today <span>&#8231;</span> {theDayWeek}
+					</span>
+					<button type="submit">Add Task</button>
+					<button onClick={() => setplusAddTaskView(true)}>Canceal</button>
+
+					{today}
+				</form>
+			)}
 		</div>
 	);
 };
